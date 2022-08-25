717639892373fc4753c4ede32d1f0f851a7311e2
Deepak Jack <93731368+DeepakJack007@users.noreply.github.com>
2022-08-25 10:18:49 +0530

    Update bot_utils.py

--- bot/helper/ext_utils/bot_utils.py
+++ bot/helper/ext_utils/bot_utils.py
@@ -29,16 +29,16 @@ PAGE_NO = 1
 
 class MirrorStatus:
     if EMOJI_THEME is True:
-    STATUS_UPLOADING = "Uploading...📤"
-    STATUS_DOWNLOADING = "Downloading...📥"
-    STATUS_CLONING = "Cloning...♻️"
-    STATUS_WAITING = "Queued...💤"
-    STATUS_PAUSED = "Paused...⛔️"
-    STATUS_ARCHIVING = "Archiving...🔐"
-    STATUS_EXTRACTING = "Extracting...📂"
-    STATUS_SPLITTING = "Splitting...✂️"
-    STATUS_CHECKING = "CheckingUp...📝"
-    STATUS_SEEDING = "Seeding...🌧"
+        STATUS_UPLOADING = "Uploading...📤"
+        STATUS_DOWNLOADING = "Downloading...📥"
+        STATUS_CLONING = "Cloning...♻️"
+        STATUS_WAITING = "Queued...💤"
+        STATUS_PAUSED = "Paused...⛔️"
+        STATUS_ARCHIVING = "Archiving...🔐"
+        STATUS_EXTRACTING = "Extracting...📂"
+        STATUS_SPLITTING = "Splitting...✂️"
+        STATUS_CHECKING = "CheckingUp...📝"
+        STATUS_SEEDING = "Seeding...🌧"
     else:
         STATUS_UPLOADING = "Upload"
         STATUS_DOWNLOADING = "Download"
